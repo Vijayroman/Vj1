c00e1352352702f46755712b9afd61b359de6ed8
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2018-08-09 12:44:35 +0530

    Update Oddeven.java

--- Oddeven.java
+++ Oddeven.java
@@ -4,18 +4,18 @@ class Odd
  public static void main(String args[])
    {
      int a;
-     Scanner a=new Scanner(System.in)
+     Scanner a=new Scanner(System.in);
        if(a%2==0)
         {
-         System.out.println('even');
+         System.out.println("even");
         }
        elseif(a%2!=0)
           {
-         System.out.println('odd');
+         System.out.println("odd");
         }
        else(a<0)
          {
-         System.out.println('Negative');
+         System.out.println("Negative");
         }
     }
    }
