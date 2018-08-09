5c460e4ed5f63e4c50af41f457a10acea210638e
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2018-08-09 12:30:15 +0530

    Create Oddeven.java

---
+++ Oddeven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Odd
+{
+ public static void main(String args[])
+   {
+     int a;
+     Scanner a=new Scanner(System.in)
+       if(a%2==0)
+        {
+         System.out.println('even');
+        }
+       else
+          {
+         System.out.println('odd');
+        }
+       }
+   }
